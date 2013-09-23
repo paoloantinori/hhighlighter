2da829ada32fc5449866b10faef7a623ae71ec52
Paolo Antinori <pantinor@redhat.com>
2013-09-23 10:32:01 +0100

    Enhanced compatibility removing function keyword

--- h.sh
+++ h.sh
@@ -6,16 +6,16 @@
 #   * or just copy and paste the function in your .bashrc
 
 #### colorize helper script that uses ack (ack-grep)
-function h() {
-	_i=0
+h() {
+	local _i=0
 	#inverted colors last scheme
-	_COLORS=( "underline bold red" "underline bold green" "underline bold yellow"  "underline bold blue"  "underline bold magenta" "bold on_red" "bold on_green" "bold black on_yellow" "bold on_blue" "bold on_magenta" "bold on_cyan" "bold black on_white"	)
+	local _COLORS=( "underline bold red" "underline bold green" "underline bold yellow"  "underline bold blue"  "underline bold magenta" "bold on_red" "bold on_green" "bold black on_yellow" "bold on_blue" "bold on_magenta" "bold on_cyan" "bold black on_white"	)
 	#inverted colors first scheme
 	#_COLORS=( "bold on_red" "bold on_green" "bold black on_yellow" "bold on_blue" "bold on_magenta" "bold on_cyan" "bold black on_white"  "underline bold red" "underline bold green" "underline bold yellow"  "underline bold blue"  "underline bold magenta" 	)
-	
+
 	for keyword in "$@"
 	do
-		_COMMAND=$_COMMAND"ack --flush --passthru --color --color-match=\"${_COLORS[$_i]}\" $keyword |"
+		local _COMMAND=$_COMMAND"ack --flush --passthru --color --color-match=\"${_COLORS[$_i]}\" $keyword |"
 	    _i=$_i+1
 	done
 	#trim ending pipe
